d6571bc315f2d61dafceb35f8a93e05711b09d5c
efekanciloglu <iletisim@efekanciloglu.com>
2026-02-06 16:46:10 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+# ---------------------------------------------------------
+# Python Masterclass - Birebir Mentörlük
+# Proje 01: Temel Veri Tipleri ve Operatörler
+# Öğrenci Adı: [Buraya Adını Yaz]
+# ---------------------------------------------------------
+
+# --- BÖLÜM 1: GİRİŞ VE VERİ ALMA ---
+print("--- YOLCULUK HESAPLAYICI BAŞLATILIYOR ---")
+
+
+# --- BÖLÜM 2: HESAPLAMALAR ---
+print("\n--- HESAPLAMA YAPILIYOR ---")
+
+
+# --- BÖLÜM 3: SONUÇLARI YAZDIRMA ---
+print("\n--- RAPOR ---")
